da1786615c3880f47757461f977d86deeec54099
Dimitris Tsiktsiris <ditsikts@outlook.com>
2020-09-22 19:17:51 +0300

    add some comments

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -12,26 +12,32 @@ import (
 
 func main() {
 	var c reqs.Conf
+	//load yaml file to Conf
 	c.GetConf()
 	keeper := make(map[string]string)
 
 	re := regexp.MustCompile("\\${\\w+}")
 
+	//we iterate through test sets
 	for i := 0; i < len(c.TestSets); i++ {
 
+		//check if there is dynamic variable which need to be setted
+		//we do it with regex re and search for this form ${mplampla}
 		if len(re.FindString(c.TestSets[i].Request.Url)) > 0 {
 			splt := re.FindAllString(c.TestSets[i].Request.Url, -1)
 
 			for _, s := range splt {
+				//we replase ${mplampla} with keeper['mplampla']
 				c.TestSets[i].Request.Url = strings.Replace(c.TestSets[i].Request.Url, s, keeper[s[2:len(s)-1]], -1)
 			}
 		}
 
+		//we do request
 		resp, err := c.TestSets[i].Request.Resolve()
 		if err != nil {
 			// handle error
 		}
-
+		//check if there is anything to keep
 		if len(c.TestSets[i].Keep) > 0 {
 
 			body, readErr := ioutil.ReadAll(resp.Body)
@@ -48,12 +54,13 @@ func main() {
 			//fmt.Println(result)
 
 			for k, v := range c.TestSets[i].Keep {
+				//extrack value return value we want to keep
 				keeper[k] = reqs.ExtractValue(result, v)
 				fmt.Println("we keep: ", keeper[k])
 			}
 
 		}
-
+		//assert happens here, currently only status
 		reqs.Assert(c.TestSets[i].Expect, resp)
 	}
 

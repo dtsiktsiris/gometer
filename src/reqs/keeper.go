319c009ed60b70a679107dd854795ffeab2e84e6
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2020-12-12 09:27:04 +0200

    polishing keeper

--- src/reqs/keeper.go
+++ src/reqs/keeper.go
@@ -8,13 +8,9 @@ import (
 func ExtractValue(respBody map[string]interface{}, path string) string {
 	var keep string
 	pathKeys := strings.Split(path, " ")
-	var temp map[string]interface{}
+	temp := respBody
 	for z := 0; z < len(pathKeys); z++ {
-		if len(pathKeys) == 1 {
-			keep = fmt.Sprintf("%v", respBody[pathKeys[z]])
-		} else if z == 0 {
-			temp = respBody[pathKeys[z]].(map[string]interface{})
-		} else if z == len(pathKeys)-1 {
+		if z == len(pathKeys)-1 {
 			keep = fmt.Sprintf("%v", temp[pathKeys[z]])
 		} else {
 			temp = temp[pathKeys[z]].(map[string]interface{})

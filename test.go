bf14561955e4fc6aa6e35a621d9d0ce7b3e5d996
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2021-02-03 08:44:52 +0200

    restructuring app

---
+++ test.go
@@ -0,0 +1,38 @@
+package gometer
+
+import (
+	"fmt"
+	"sync"
+)
+
+func handleTests(tests []Test, keeper map[string]string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for testIndex, test := range tests {
+
+		var assRes []string
+		assRes = append(assRes, fmt.Sprintf("*** Test: %v", testIndex+1))
+
+		//check if there is dynamic variable which need to be setted
+		//we do it with regex re and search for this form ${mplampla}
+		setDynamicVariables(&test.Request, keeper)
+
+		//we do request
+		result := test.Request.GetRequestResult()
+
+		//check what to keep
+		for k, v := range test.Keep {
+			//extractValue return value we want to keep
+			//v is the path to this value
+			keeper[k] = ExtractValue(result, v)
+			// fmt.Println("we keep: ", keeper[k.VariableName])
+		}
+		//assert happens here
+		assRes = Assert(test.Expect, result, assRes)
+
+		for _, s := range assRes {
+			fmt.Println(s)
+		}
+
+	}
+
+}
